bfc59d7e0a4889a706c527fae7ef3a0501649c1d
Artic030 <030ci@mail.ru>
2021-07-18 11:41:26 +0300

    Console Test add

---
+++ Tests/EnhancedConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnhancedConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
