ab8fb45021c2d9a6ef2aa0a402b69f1507d53073
Artic030 <030ci@mail.ru>
2021-07-18 12:55:12 +0300

    Base command class

--- EnhancedTestApp/ViewModels/MainWindowViewModel.cs
+++ EnhancedTestApp/ViewModels/MainWindowViewModel.cs
@@ -7,12 +7,25 @@ namespace EnhancedTestApp.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Свойство заголовка
         /// <summary> Заголовок окна </summary>
         private string _Title = "Главное меню";
-        public string Title 
-        { 
-            get => _Title; 
-            set => Set(ref _Title, value); 
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
         }
+        #endregion
+
+        #region Свойство статус
+        /// <summary> Заголовок окна </summary>
+        private string _Status = "Something...";
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+        #endregion
+
     }
 }
