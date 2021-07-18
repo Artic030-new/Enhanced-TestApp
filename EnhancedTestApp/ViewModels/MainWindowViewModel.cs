7d77b81db6a52fdb9f266ea5105ba1abdcc429c4
Artic030 <030ci@mail.ru>
2021-07-18 12:25:23 +0300

    vm test

--- EnhancedTestApp/ViewModels/MainWindowViewModel.cs
+++ EnhancedTestApp/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using EnhancedTestApp.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EnhancedTestApp.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
-
+        /// <summary> Заголовок окна </summary>
+        private string _Title = "Главное меню";
+        public string Title 
+        { 
+            get => _Title; 
+            set => Set(ref _Title, value); 
+        }
     }
 }
