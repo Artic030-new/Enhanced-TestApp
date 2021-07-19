a4770275066d363a7db3d1c6aeb3fc1e1f7405fd
Artic030 <030ci@mail.ru>
2021-07-19 21:07:54 +0300

    Add http get statistics

--- Tests/EnhancedConsole/Program.cs
+++ Tests/EnhancedConsole/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace EnhancedConsole
 {
     class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            var csv_str = client.GetAsync(data_url).Result;
+            var csv_str2 = csv_str.Content.ReadAsStringAsync().Result;
         }
     }
 }
