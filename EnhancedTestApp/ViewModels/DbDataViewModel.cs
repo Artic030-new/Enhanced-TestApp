05ff77adebe2d3afd725b08a5bbb39efaa123f0f
Artic030 <030ci@mail.ru>
2021-07-24 21:38:13 +0300

    Add db window vm

---
+++ EnhancedTestApp/ViewModels/DbDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EnhancedTestApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedTestApp.ViewModels
+{
+    class DbDataViewModel : ViewModel
+    {
+        private string _Title = "Главное меню";
+        public string Title {get => _Title; set => Set(ref _Title, value); }
+    }
+}
