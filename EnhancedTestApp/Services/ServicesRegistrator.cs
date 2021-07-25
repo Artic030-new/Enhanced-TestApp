b336e7a0157b85696cf19f89f79e976624380646
Artic030 <030ci@mail.ru>
2021-07-25 19:51:06 +0300

    Add registrators

---
+++ EnhancedTestApp/Services/ServicesRegistrator.cs
@@ -0,0 +1,15 @@
+﻿using EnhancedTestApp.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedTestApp.Services
+{
+    static class ServicesRegistrator
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services) => services
+            .AddSingleton<MainWindowViewModel>()
+        ;
+    }
+}
